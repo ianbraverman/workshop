b28e67822b4e4ee005b58048c0dae8f8262d31c2
Ian <ianbraverman@hotmail.com>
2024-02-22 15:30:53 -0500

    finished the puppies.jsx file so that it should get all the information from the puppies

--- block27Cpuppybowl/src/puppies.jsx
+++ block27Cpuppybowl/src/puppies.jsx
@@ -1,3 +1,30 @@
 import { useGetPuppiesQuery } from "./puppiesSlice";
 
-export default function Recipes() {}
+export default function Puppies() {
+  const { data: puppies, isLoading } = useGetPuppiesQuery();
+
+  return (
+    <article>
+      <h1>Welcome to the Puppy Bowl Event!</h1>
+      <p> Come and join us for a fun-filled day of adorable puppy football!</p>
+      <p>
+        Get ready to meet the talented pups competing in this year's Puppy
+        Bowl... and here they are:
+      </p>
+      <ul>
+        {isLoading ? (
+          <li>Loading...</li>
+        ) : (
+          puppies.data.map((puppy) => {
+            <li key={puppy.id}>
+              <img src={puppy.imageUrl} />
+              <h2>{puppy.name}</h2>
+              <h3>{puppy.breed}</h3>
+              <h3>{puppy.status}</h3>
+            </li>;
+          })
+        )}
+      </ul>
+    </article>
+  );
+}
